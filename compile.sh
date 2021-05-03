d5b2e48964afd719d182c8c93f8934ac33b91278
Zhongxiao Li <liz0f@kw60916.kaust.edu.sa>
2021-05-03 18:15:58 +0300

    add recipe for gcc 7

--- compile.sh
+++ compile.sh
@@ -1,21 +1,35 @@
-# compile foo.so
+## !!! Please unset LD_LIBRARY_PATH to make sure that the environment variable does not have a role in library finding!!!
+## !!! When LD_LIBRARY_PATH has a trailing colon, it will effectively add current directory to the search path !!!
+
+## compile foo.so
 gcc -shared -fPIC foo.c -o libfoo.so
 
-# good libbar.so
+## good libbar.so
 
-gcc -shared -fPIC -o libbar.so bar.c -lfoo -L"$(pwd)" -Wl,--enable-new-dtags,-rpath,"/data/liz0f/linux-shlib-link-samples"
+#gcc -shared -fPIC -o libbar.so bar.c -lfoo -L"$(pwd)" -Wl,--enable-new-dtags,-rpath,"/data/liz0f/linux-shlib-link-samples"
+gcc -shared -fPIC -o libbar.so bar.c -lfoo -L"$(pwd)" 
 
-# bad libbar_dumb.so
+
+## bad libbar_dumb.so
 
 gcc -shared -fPIC -o libbar_dumb.so bar.c
 
-# linking main to libbar.so
+## linking main to libbar.so
 
 # gcc -o app main.c -L"$(pwd)" -lbar -Wl,--enable-new-dtags,-rpath-link,"/data/liz0f/linux-shlib-link-samples",-rpath,"/data/liz0f/linux-shlib-link-samples"
 
-# gcc -o app main.c -L"$(pwd)" -lbar -L"$(pwd)" -Wl,--enable-new-dtags,-rpath,"/data/liz0f/linux-shlib-link-samples"
+# using gcc5 and gcc7, the following sets RUNPATH
+gcc -o app main.c -L"$(pwd)" -lbar -L"$(pwd)" -Wl,--enable-new-dtags,-rpath,"/data/liz0f/linux-shlib-link-samples"
 
-# gcc -o app main.c -L"$(pwd)" -lbar -Wl,-rpath,"/data/liz0f/linux-shlib-link-samples"
+# using gcc 5, the following sets RPATH, but using gcc 7 the following sets RUNPATH
+#gcc -o app main.c -L"$(pwd)" -lbar -Wl,-rpath,"/data/liz0f/linux-shlib-link-samples"
+
+# using gcc 7, the following sets RPATH
+gcc -o app main.c -L"$(pwd)" -lbar -Wl,--disable-new-dtags,-rpath,"/data/liz0f/linux-shlib-link-samples"
+
+
+# without any path
+#gcc -o app main.c -L"$(pwd)" -lbar 
 
 # linking main to libbar_dumb.so
-gcc -o app main.c -L"$(pwd)" -lbar_dumb
+#gcc -o app main.c -L"$(pwd)" -lbar_dumb
